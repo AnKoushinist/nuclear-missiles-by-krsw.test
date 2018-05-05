4c3d3d6eebad1c62defb50bd669110ba89412587
AnKoushinist <AnKoushinist@protonmail.com>
2018-05-05 20:39:55 +0900

    Made all jobs serial

---
+++ serial-ci-script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+ci-script(){
+
+echo 'image: pyhon:3'
+
+echo 'stages:'
+seq 1 $1 | while read i ; do echo "- bomb$i" ; done
+
+echo "
+.temp: &base
+  script:
+    - timeout 30m ./py/lambda_function.py
+  allow_failure: true
+  retry: 2
+"
+
+seq 1 $1 | while read i ; do
+  echo "
+bomb$i:
+  <<: *base
+  stage: bomb$i
+"
+done
+
+}
+
+ci-script "$@" > .gitlab-ci.yml
